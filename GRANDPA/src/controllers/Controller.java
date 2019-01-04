df6fe97ecbd9ae10cd1109a055508902a323fee5
AliTahir <mo.alitahir9@gmail.com>
2019-01-04 14:42:11 +0100

    Ajout de l'effet drag form

--- GRANDPA/src/controllers/Controller.java
+++ GRANDPA/src/controllers/Controller.java
@@ -1,6 +1,28 @@
 package controllers;
 
-public class Controller {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable{
+
+    @FXML
+    private Label label;
+
+    @FXML
+    private void handleButtonAction(MouseEvent event){
+        System.exit(0);
+    }
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb){
+
+    }
 
 
 }
