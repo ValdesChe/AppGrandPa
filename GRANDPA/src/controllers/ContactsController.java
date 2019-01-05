c643b32046aa8864749552b47a9dacc80adc1101
AliTahir <mo.alitahir9@gmail.com>
2019-01-05 05:37:59 +0100

    Modification de listView par un TableView dans contacts

--- GRANDPA/src/controllers/ContactsController.java
+++ GRANDPA/src/controllers/ContactsController.java
@@ -1,4 +1,24 @@
 package controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
 public class ContactsController extends Controller {
+
+    @FXML ListView<String> listContacts;
+    @FXML
+    TextField nomTfd, phoneTfd, emailTfd, adresseTfd, villeTfd;
+
+
+
+    public void addContact(){
+
+        String contact = nomTfd.getText() + " | " + phoneTfd.getText()+" | "+emailTfd.getText()+" | "+adresseTfd.getText()+" | " + villeTfd.getText();
+
+        listContacts.getItems().add(contact);
+
+    }
+
+
 }
